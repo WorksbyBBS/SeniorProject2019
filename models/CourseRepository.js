3c7ccda8915400f6ebf86bbd7d1728e03e819a0a
Alo <bbscydia@gmail.com>
2019-03-10 00:01:59 +0300

    removed comment

--- models/CourseRepository.js
+++ models/CourseRepository.js
@@ -92,71 +92,8 @@ class CourseRepository {
             ctArray.push(ct)
         }
         return ctArray;
-        // let ct = await Course_Trainee_Trainer.update({
-        //     trainer_id: trainer_id
-        // }, {where: {course_id: course_id}});
-        //
-        // return ct;
+        
     }
-
-    // async assignCourseTrainee(course_id, selectedTraineesIDs) {
-    //
-    //     //if >1, update the one record that exists (if null), then insert the rest.
-    //     //if >1 and record is not null, insert the others
-    //     // If only 1, update the record that exists
-    //     if (selectedTraineesIDs.length > 1) {
-    //
-    //         //find existing record with course_id
-    //         let existingRecord = await Course_Trainee_Trainer.findOne({course_id: course_id});
-    //
-    //         //find existing record with course_id where the trainee_id is null
-    //         let query = 'select * from sp2019_db.Course_Trainee_Trainers where course_id=' + course_id + ' and trainee_id is null;';
-    //         let existingRecordWithoutTrainee = await Course_Trainee_Trainer.findOne({
-    //             course_id: course_id,
-    //             trainee_id: -1
-    //         })
-    //         console.log(existingRecordWithoutTrainee);
-    //
-    //         //it exists with a trainee, so insert others
-    //         if (existingRecordWithoutTrainee !== null) {
-    //             let ctArray = [];
-    //             for (let i = 0; i < selectedTraineesIDs.length; i++) {
-    //                 ctArray.push(await Course_Trainee_Trainer.create({
-    //                     trainee_id: selectedTraineesIDs[i],
-    //                     trainer_id: existingRecord.trainer_id,
-    //                     course_id: existingRecord.course_id
-    //                 }, {where: {course_id: course_id}}));
-    //             }
-    //             return ctArray;
-    //
-    //             //it doesn't have a trainee, so update it with the first trainee and insert the rest
-    //         } else {
-    //             //update
-    //             let ct = await Course_Trainee_Trainer.update({
-    //                 trainee_id: selectedTraineesIDs[0]
-    //             }, {where: {course_id: course_id}});
-    //             let ctArray = [];
-    //
-    //
-    //             ctArray.push(ct);
-    //             //insert rest
-    //             for (let i = 1; i < selectedTraineesIDs.length; i++) {
-    //                 ctArray.push(await Course_Trainee_Trainer.create({
-    //                     trainee_id: selectedTraineesIDs[i],
-    //                     trainer_id: existingRecord.trainer_id,
-    //                     course_id: existingRecord.course_id
-    //                 }, {where: {course_id: course_id}}));
-    //             }
-    //
-    //             return ctArray;
-    //         }
-    //     } else {
-    //         let ct = await Course_Trainee_Trainer.update({
-    //             trainee_id: selectedTraineesIDs[0]
-    //         }, {where: {course_id: course_id}});
-    //         return ct;
-    //     }
-    // }
 
     async addSkill(skillObj) {
         return await Skills.create({
